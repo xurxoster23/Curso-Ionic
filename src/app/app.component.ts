66d7832866c6f67e6bf585aa9d8c4694b6a8e118
xurxoster23 <xurxoster@gmail.com>
2022-11-15 15:28:16 +0100

    Seccion 5 Componente ion-menu2

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { StatusBar } from '@capacitor/status-bar';
+import { Platform } from '@ionic/angular';
+import { Observable } from 'rxjs';
+import { Componente } from './interfaces/interfaces';
+import { DataService } from './services/data.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',
   styleUrls: ['app.component.scss'],
 })
-export class AppComponent {
-  constructor() {}
+export class AppComponent implements OnInit {
+
+  componentes: Observable<Componente[]>;
+
+  constructor(private dataService: DataService ) {}
+
+  ngOnInit(){
+    this.componentes = this.dataService.getMenuOpt();
+  }
+
+
 }
+
+
+
