c5cbe40776773eb0301b9117c6ffc76a65bd59ef
xurxoster23 <xurxoster@gmail.com>
2022-11-16 09:28:49 +0100

    Sección 5 Componentes ion-progress-bar y ion-range

---
+++ src/app/pages/progress/progress.page.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-progress',
+  templateUrl: './progress.page.html',
+  styleUrls: ['./progress.page.scss'],
+})
+export class ProgressPage implements OnInit {
+
+
+  progreso = 0.05;
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  rangeChange(event){
+    this.progreso = (event.detail.value) / 100;
+    console.log(this.progreso);
+  }
+
+}
